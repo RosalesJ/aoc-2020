3acc42cb015a6f98de46513ec0f1da74eab8fb64
Jacob Rosales Chase <rosaleschase.j@gmail.com>
2020-12-01 18:50:12 -0500

    Finish 1b

--- src/aoc1/b/1b.ml
+++ src/aoc1/b/1b.ml
@@ -1,24 +1,39 @@
 open Base
 open Stdio
 
-let report_repair xs =
+let report_repair xs i =
   let target = 2020 in
-  let arr = Array.of_list xs in
-  Array.sort ~compare:Int.compare arr;
-
-  let rec loop arr i j =
-    match arr.(i) + arr.(j) with
-    | x when x = target -> arr.(i) * arr.(j)
-    | x when x > target -> loop arr i (j - 1)
-    | x when x < target -> loop arr (i + 1) j
-    | _ when i = j      -> -1
+  
+  let rec loop arr i j k =
+    Stdio.printf "%d, %d, %d:   %d\n" i j k (arr.(i) + arr.(j) + arr.(k));
+    match arr.(i) + arr.(j) + arr.(k) with
+    | _ when j = k      -> -1
+    | x when x = target -> arr.(i) * arr.(j) * arr.(k)
+    | x when x > target -> loop arr i j (k - 1)
+    | x when x < target -> loop arr i (j + 1) k
     | _                 -> -1
   in
-  loop arr 0 (Array.length arr - 1)
+  loop xs i (i + 1) (Array.length xs - 1)
+
+let report_repair3 xs =
+  Array.sort ~compare:Int.compare xs;
+
+  let rec loop i =
+    if i >= Array.length xs - 4 then -1
+    else 
+      let value = report_repair xs i in
+      if value <> -1 then
+        value
+      else
+        loop (i + 1)
+  in
+  loop 0
+      
 
 let () =
   In_channel.create "./src/aoc1/input.txt"
   |> In_channel.input_lines
   |> List.map ~f:Int.of_string
-  |> report_repair
+  |> Array.of_list
+  |> report_repair3
   |> Stdio.printf "%d\n"
