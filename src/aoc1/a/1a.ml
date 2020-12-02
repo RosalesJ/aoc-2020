716b06bb73aa28605343aede7297c87068c66b3d
Jacob Rosales Chase <rosaleschase.j@gmail.com>
2020-12-01 19:08:40 -0500

    A little cleanup

--- src/aoc1/a/1a.ml
+++ src/aoc1/a/1a.ml
@@ -1,24 +1,25 @@
 open Base
 open Stdio
 
-let report_repair xs =
-  let target = 2020 in
-  let arr = Array.of_list xs in
+let report_repair target arr =
   Array.sort ~compare:Int.compare arr;
 
   let rec loop arr i j =
     match arr.(i) + arr.(j) with
+    | _ when i = j      -> -1
     | x when x = target -> arr.(i) * arr.(j)
     | x when x > target -> loop arr i (j - 1)
     | x when x < target -> loop arr (i + 1) j
-    | _ when i = j      -> -1
     | _                 -> -1
   in
   loop arr 0 (Array.length arr - 1)
 
 let () =
+  let target = 2020 in
+
   In_channel.create "./src/aoc1/input.txt"
   |> In_channel.input_lines
   |> List.map ~f:Int.of_string
-  |> report_repair
+  |> Array.of_list
+  |> report_repair target
   |> Stdio.printf "%d\n"
--- src/aoc1/b/1b.ml
+++ src/aoc1/b/1b.ml
@@ -1,39 +1,35 @@
 open Base
 open Stdio
 
-let report_repair xs i =
-  let target = 2020 in
-  
-  let rec loop arr i j k =
-    Stdio.printf "%d, %d, %d:   %d\n" i j k (arr.(i) + arr.(j) + arr.(k));
+let report_repair target arr k =
+  let rec loop arr i j =
     match arr.(i) + arr.(j) + arr.(k) with
-    | _ when j = k      -> -1
-    | x when x = target -> arr.(i) * arr.(j) * arr.(k)
-    | x when x > target -> loop arr i j (k - 1)
-    | x when x < target -> loop arr i (j + 1) k
-    | _                 -> -1
+    | _ when i = j      -> None
+    | x when x = target -> Some (arr.(i) * arr.(j) * arr.(k))
+    | x when x > target -> loop arr i (j - 1)
+    | x when x < target -> loop arr (i + 1) j
+    | _                 -> None
   in
-  loop xs i (i + 1) (Array.length xs - 1)
+  loop arr (k + 1) (Array.length arr - 1)
 
-let report_repair3 xs =
+let report_repair3 target xs =
   Array.sort ~compare:Int.compare xs;
 
   let rec loop i =
     if i >= Array.length xs - 4 then -1
-    else 
-      let value = report_repair xs i in
-      if value <> -1 then
-        value
-      else
-        loop (i + 1)
+    else match report_repair target xs i with
+    | None -> loop (i + 1)
+    | Some x -> x
   in
   loop 0
       
 
 let () =
+  let target = 2020 in
+
   In_channel.create "./src/aoc1/input.txt"
   |> In_channel.input_lines
   |> List.map ~f:Int.of_string
   |> Array.of_list
-  |> report_repair3
+  |> report_repair3 target
   |> Stdio.printf "%d\n"
