d887b6c145b2917bd33d979ba5ee72fdf0b81fbb
Jacob Rosales Chase <rosaleschase.j@gmail.com>
2020-12-05 23:48:30 -0500

    Cleanup for 5

--- src/aoc5/a/5a.ml
+++ src/aoc5/a/5a.ml
@@ -1,56 +1,52 @@
 open Core
 open Stdio
 
-type seat = { row: int; column: int }
+type seat = { row: int; column: int; mutable occupied: bool }
 
 type row = Subrow of { left: row; right: row } | Seat of seat
 
 type plane = Section of { front: plane; back: plane } | Row of row
 
-let show_seat { row; column } = sprintf "(%d,%d)" row column
-let seat_id { row; column } = row * 8 + column
+let new_seat row column = { row; column; occupied = false }
+
+let seat_id { row; column; _ } = row * 8 + column
 
 let split xs = List.groupi xs ~break:(fun i _ _ -> i = List.length xs / 2)
-
-let rec show_row row =
-  match row with
-  | Seat s -> show_seat s
-  | Subrow {left; right} -> sprintf "%s %s" (show_row left) (show_row right)
-
-let rec show plane =
-  match plane with
-  | Row r -> show_row r
-  | Section {front; back} -> sprintf "%s\n%s" (show front) (show back)
 
 let rec seats_to_row seats =
   match split seats with
   | [[seat]]      -> Seat seat
-  | [left; right] -> Subrow { left = seats_to_row left; right = seats_to_row right }
+  | [left; right] -> Subrow { left = seats_to_row left;
+                              right = seats_to_row right }
   | _             -> failwith "Couldn't make seats into rows"
 
 let rec rows_to_section rows =
   match split rows with
   | [[row]]       -> Row row
-  | [front; back] -> Section { front = rows_to_section front; back = rows_to_section back }
+  | [front; back] -> Section { front = rows_to_section front;
+                               back = rows_to_section back }
   | _             -> failwith "Couldn't make rows into sections"
 
 let build_plane ~rows ~cols =
-  let seats = List.init rows ~f:(fun row -> List.init cols ~f:(fun column -> { row; column })) in
+  let seats_for_row row_num = List.init cols ~f:(new_seat row_num) in
+  let seats = List.init rows ~f:seats_for_row in
   let rows = List.map seats ~f:seats_to_row in
   let plane = rows_to_section rows in
   plane
 
 let decode_seat plane encoding =
   let rec get_row section i =
-    if i > 7 then failwith (sprintf "row instruction out of bounds: '%d'" i);
+    if i > 7 then
+      failwith (sprintf "row instruction out of bounds: '%d'" i);
     match String.get encoding i, section with
     | 'B', Section { back; _ }  -> get_row back (i + 1)
     | 'F', Section { front; _ } -> get_row front (i + 1)
     | _,   Row row when i = 7   -> row
-    | x, _                         -> failwith (sprintf "Failed getting row at %c, %d" x i)
+    | x, _  -> failwith (sprintf "Failed getting row at %c, %d" x i)
   in
   let rec get_seat row i =
-    if i <= 6 || 10 < i then failwith (sprintf "seat instruction out of bounds: '%d'" i);
+    if i <= 6 || 10 < i then
+      failwith (sprintf "Seat instruction out of bounds: '%d'" i);
     match row with
     | Seat st when i = 10  -> st
     | Seat _ -> failwith "Found a seat too early!"
@@ -72,12 +68,7 @@ let binary_boarding seat_codings =
   |> function None -> 0 | Some x -> x
 
 let () =
-try
-  (* build_plane ~cols:8 ~rows:128
-  |> show
-  |> printf "%s\n"; *)
   In_channel.create "./src/aoc5/input.txt"
   |> In_channel.input_lines
   |> binary_boarding
   |> Stdio.printf "%d\n"
-with Failure f -> printf "I'm dumb: %s" f
--- src/aoc5/b/5b.ml
+++ src/aoc5/b/5b.ml
@@ -7,51 +7,46 @@ type row = Subrow of { left: row; right: row } | Seat of seat
 
 type plane = Section of { front: plane; back: plane } | Row of row
 
-let show_seat { row; column; occupied } = sprintf "(%d,%d,%b)" row column occupied
+let new_seat row column = { row; column; occupied = false }
+
 let seat_id { row; column; _ } = row * 8 + column
-let show_seat_id seat = sprintf "(%d,%b)" (seat_id seat) seat.occupied
 
 let split xs = List.groupi xs ~break:(fun i _ _ -> i = List.length xs / 2)
-
-let rec show_row row =
-  match row with
-  | Seat s -> show_seat_id s
-  | Subrow {left; right} -> sprintf "%s %s" (show_row left) (show_row right)
-
-let rec show plane =
-  match plane with
-  | Row r -> show_row r
-  | Section {front; back} -> sprintf "%s\n%s" (show front) (show back)
 
 let rec seats_to_row seats =
   match split seats with
   | [[seat]]      -> Seat seat
-  | [left; right] -> Subrow { left = seats_to_row left; right = seats_to_row right }
+  | [left; right] -> Subrow { left = seats_to_row left;
+                              right = seats_to_row right }
   | _             -> failwith "Couldn't make seats into rows"
 
 let rec rows_to_section rows =
   match split rows with
   | [[row]]       -> Row row
-  | [front; back] -> Section { front = rows_to_section front; back = rows_to_section back }
+  | [front; back] -> Section { front = rows_to_section front;
+                               back = rows_to_section back }
   | _             -> failwith "Couldn't make rows into sections"
 
 let build_plane ~rows ~cols =
-  let seats = List.init rows ~f:(fun row -> List.init cols ~f:(fun column -> { row; column; occupied = false })) in
+  let seats_for_row row_num = List.init cols ~f:(new_seat row_num) in
+  let seats = List.init rows ~f:seats_for_row in
   let rows = List.map seats ~f:seats_to_row in
   let plane = rows_to_section rows in
   plane, seats
 
 let decode_seat plane encoding =
   let rec get_row section i =
-    if i > 7 then failwith (sprintf "row instruction out of bounds: '%d'" i);
+    if i > 7 then
+      failwith (sprintf "row instruction out of bounds: '%d'" i);
     match String.get encoding i, section with
     | 'B', Section { back; _ }  -> get_row back (i + 1)
     | 'F', Section { front; _ } -> get_row front (i + 1)
     | _,   Row row when i = 7   -> row
-    | x, _                         -> failwith (sprintf "Failed getting row at %c, %d" x i)
+    | x, _  -> failwith (sprintf "Failed getting row at %c, %d" x i)
   in
   let rec get_seat row i =
-    if i <= 6 || 10 < i then failwith (sprintf "seat instruction out of bounds: '%d'" i);
+    if i <= 6 || 10 < i then
+      failwith (sprintf "Seat instruction out of bounds: '%d'" i);
     match row with
     | Seat st when i = 10  -> st
     | Seat _ -> failwith "Found a seat too early!"
@@ -63,7 +58,8 @@ let decode_seat plane encoding =
   in
   let row = get_row plane 0 in
   let seat = get_seat row 7 in
-  seat.occupied <- true
+  seat.occupied <- true;
+  seat_id seat
 
 let is_my_seat set x =
   let id = seat_id x in
@@ -74,15 +70,12 @@ let is_my_seat set x =
 let binary_boarding seat_codings =
   let plane, seats = build_plane ~cols:8 ~rows:128 in
 
-  List.iter ~f:(decode_seat plane) seat_codings;
-
-  let set = List.fold
-      (List.concat seats)
-      ~init:(Set.empty (module Int))
-      ~f:(fun acc x -> if x.occupied then Set.add acc (seat_id x) else acc)
+  let occupied =
+    let seat_ids = List.map ~f:(decode_seat plane) seat_codings in
+    List.fold seat_ids ~f:Set.add ~init:(Set.empty (module Int))
   in
 
-  List.find (List.concat seats) ~f:(is_my_seat set)
+  List.find (List.concat seats) ~f:(is_my_seat occupied)
   |> function None -> 0 | Some x -> seat_id x
 
 let () =
