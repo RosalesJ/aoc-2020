7f6b84137441cd4d5059b684aabced822bba18e4
Jacob Rosales Chase <rosaleschase.j@gmail.com>
2020-12-11 00:42:43 -0500

    Finish 10b in a linear time and constant space

--- src/aoc10/b/10b.ml
+++ src/aoc10/b/10b.ml
@@ -4,13 +4,24 @@ open Stdio
 let adapter_array input =
   let input = List.sort input ~compare:Int.compare in
 
-  let rec loop acc last rest =
+  (*  .. x0, x1, x2, x3 ...xs*)
+  let rec loop x0 x1 x2 s0 s1 s2 rest =
     match rest with
-    | _ :: [] | [] -> acc + 1
-    | x :: (x1 :: _ as xs) when x1 - last <= 3 -> loop acc x xs + loop acc last xs
-    | x :: xs -> loop acc x xs
+    | [] -> s2 + (if x2 +3 - x2 <= 3 && x2 + 3 - x1 <= 3 then s1 + (if x2+3 - x0 <= 3 then s0 else 0) else 0)
+    | x3 :: xs ->
+(*       printf "x(%d %d %d) %d\ns(%d %d %d)\n\n" x0 x1 x2 x3 s0 s1 s2; *)
+      let scur = s2 + (if x3 - x2 <= 3 && x3 - x1 <= 3 then s1 + (if x3 - x0 <= 3 then s0 else 0) else 0) in
+      loop x1 x2 x3 s1 s2 scur xs
   in
-  loop 0 0 input
+  match input with
+  | x1 :: x2 :: xs -> begin
+      let s0 = 1 in
+      let s1 = 1 in
+      let s2 = s1 + (if x1 < 3 && x2 < 3 then s0 else 0) in
+      printf "%d %d %d\n" s0 s1 s2;
+      loop 0 x1 x2 s0 s1 s2 xs
+    end
+  | _ -> assert false
 
 let () =
   In_channel.create "./src/aoc10/input.txt"
