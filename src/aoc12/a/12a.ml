34d619a3eceae00e3776e7e01e91508f35e67b27
Jacob Rosales Chase <rosaleschase.j@gmail.com>
2020-12-12 01:13:02 -0500

    Cleanup for 12

--- src/aoc12/a/12a.ml
+++ src/aoc12/a/12a.ml
@@ -2,17 +2,18 @@ open Core
 open Stdio
 open Angstrom
 
-
 type instr = { typ: [`N | `S | `E | `W | `L | `R | `F]; num:int }
 type ship = { pos: Complex.t; dir: Complex.t }
+
 let make_instr typ num = { typ; num }
 let starting_ship = { pos = Complex.zero; dir = Complex.one }
 let taxicab { pos; _ } = abs (Int.of_float pos.re) + abs (Int.of_float pos.im)
 
-
-
 let parse_instr input =
-  let typ = any_char >>| function 'N' -> `N | 'S' -> `S | 'E' -> `E | 'W' -> `W | 'L' -> `L | 'R' -> `R | 'F' -> `F | _ -> failwith "bad char" in
+  let typ = any_char >>| function
+    | 'N' -> `N | 'S' -> `S | 'E' -> `E | 'W' -> `W | 'L' -> `L
+    | 'R' -> `R | 'F' -> `F | x -> failwith (sprintf "Bad intruction %c" x)
+  in
   let num = take_while Char.is_digit >>| Int.of_string in
   let parser = lift2 make_instr typ num in
   match parse_string (sep_by (char '\n') parser) input ~consume:All with
@@ -20,25 +21,29 @@ let parse_instr input =
   | Error x -> printf "%s\n" x; []
 
 let interperet ship { typ; num } =
-  printf "%f %f %f %f\n" ship.pos.im ship.pos.re ship.dir.im ship.dir.re;
   let open Complex in
-  let (+) = Complex.add in
-  let (-) = Complex.sub in
-  let ( * ) s c = let s = Float.of_int s in Complex.{ re = c.re *. s ; im = c.im *. s;} in
+  let ( + ) = Complex.add in
+  let ( - ) = Complex.sub in
+  let ( * ) s c =
+    let s = Float.of_int s in
+    Complex.{ re = c.re *. s ; im = c.im *. s;}
+  in
+  let update_dir c num =
+    List.init ~f:(const c) (num / 90)
+    |> List.fold ~f:mul ~init:ship.dir
+  in
    match typ with
   | `N -> { ship with pos = ship.pos + num * i }
   | `S -> { ship with pos = ship.pos - num * i }
   | `E -> { ship with pos = ship.pos + num * one }
   | `W -> { ship with pos = ship.pos - num * one }
   | `F -> { ship with pos = ship.pos + num * ship.dir }
-  | `L -> let num = List.init ~f:(const i) (num / 90) in
-      { ship with dir = List.fold ~f:mul ~init:ship.dir num }
-  | `R -> let num = List.init ~f:(const (neg i)) (num / 90) in
-      { ship with dir = List.fold ~f:mul ~init:ship.dir num }
+  | `L -> { ship with dir = update_dir i num }
+  | `R -> { ship with dir = update_dir (neg i) num }
 
 let rain_risk input =
-  let instr = parse_instr input in
-  List.fold instr ~init:starting_ship ~f:interperet
+  parse_instr input
+  |> List.fold  ~init:starting_ship ~f:interperet
   |> taxicab
 
 let () =
--- src/aoc12/b/12b.ml
+++ src/aoc12/b/12b.ml
@@ -2,17 +2,18 @@ open Core
 open Stdio
 open Angstrom
 
-
 type instr = { typ: [`N | `S | `E | `W | `L | `R | `F]; num:int }
 type ship = { pos: Complex.t; waypoint: Complex.t }
+
 let make_instr typ num = { typ; num }
 let starting_ship = { pos = Complex.zero; waypoint = Complex.{ im = 1.; re = 10. } }
 let taxicab { pos; _ } = abs (Int.of_float pos.re) + abs (Int.of_float pos.im)
 
-
-
 let parse_instr input =
-  let typ = any_char >>| function 'N' -> `N | 'S' -> `S | 'E' -> `E | 'W' -> `W | 'L' -> `L | 'R' -> `R | 'F' -> `F | _ -> failwith "bad char" in
+  let typ = any_char >>| function
+    | 'N' -> `N | 'S' -> `S | 'E' -> `E | 'W' -> `W | 'L' -> `L
+    | 'R' -> `R | 'F' -> `F | x -> failwith (sprintf "Bad intruction %c" x)
+  in
   let num = take_while Char.is_digit >>| Int.of_string in
   let parser = lift2 make_instr typ num in
   match parse_string (sep_by (char '\n') parser) input ~consume:All with
@@ -20,21 +21,25 @@ let parse_instr input =
   | Error x -> printf "%s\n" x; []
 
 let interperet ship { typ; num } =
-  printf "%f %f %f %f\n" ship.pos.im ship.pos.re ship.waypoint.im ship.waypoint.re;
   let open Complex in
-  let (+) = Complex.add in
-  let (-) = Complex.sub in
-  let ( * ) s c = let s = Float.of_int s in Complex.{ re = c.re *. s ; im = c.im *. s;} in
-   match typ with
+  let ( + ) = Complex.add in
+  let ( - ) = Complex.sub in
+  let ( * ) s c =
+    let s = Float.of_int s in
+    Complex.{ re = c.re *. s ; im = c.im *. s;}
+  in
+  let update_dir c num =
+    List.init ~f:(const c) (num / 90)
+    |> List.fold ~f:mul ~init:ship.waypoint
+  in
+  match typ with
   | `N -> { ship with waypoint = ship.waypoint + num * i }
   | `S -> { ship with waypoint = ship.waypoint - num * i }
   | `E -> { ship with waypoint = ship.waypoint + num * one }
   | `W -> { ship with waypoint = ship.waypoint - num * one }
   | `F -> { ship with pos = ship.pos + num * ship.waypoint }
-  | `L -> let num = List.init ~f:(const i) (num / 90) in
-      { ship with waypoint = List.fold ~f:mul ~init:ship.waypoint num }
-  | `R -> let num = List.init ~f:(const (neg i)) (num / 90) in
-      { ship with waypoint = List.fold ~f:mul ~init:ship.waypoint num }
+  | `L -> { ship with waypoint = update_dir i num }
+  | `R -> { ship with waypoint = update_dir (neg i) num }
 
 let rain_risk input =
   let instr = parse_instr input in
