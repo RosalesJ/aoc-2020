51d44b1e930a440e3304701e5008a1c53732eba2
Jacob Rosales Chase <rosaleschase.j@gmail.com>
2020-12-13 17:27:00 -0500

    Clean up 13 a bit more

--- src/aoc13/b/13b.ml
+++ src/aoc13/b/13b.ml
@@ -1,17 +1,18 @@
 open Core
 open Stdio
 
-let rec find_time init p inc off k =
-  if (inc * k + init + off) mod p = 0 then
-    inc * k + init
-  else
-    find_time init p inc off (k + 1)
-
 let shuttle_search input =
+  let rec closest_time (step, time) (c, p) =
+    if (time + c) mod p = 0 then
+      (step * p, time)
+    else
+      closest_time (step, time + step) (c, p)
+  in
+
   String.split ~on:',' (List.nth_exn input 1)
   |> List.filter_mapi ~f:(fun i x -> try Some (i, Int.of_string x) with Failure _ -> None)
-  |> List.fold ~init:(1, 1) ~f:(fun (inc, init) (off, p) -> (inc * p, find_time init p inc off 0))
-  |> Tuple.T2.get2
+  |> List.fold ~init:(1, 1) ~f:closest_time
+  |> Tuple2.get2
 
 let () =
   In_channel.create "./src/aoc13/input.txt"
