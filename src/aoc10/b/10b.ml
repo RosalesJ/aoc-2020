8c3025f38ce7f5348dd8cb781fef05d9f9f60993
Jacob Rosales Chase <rosaleschase.j@gmail.com>
2021-01-02 20:01:27 -0500

    A little clarification

--- src/aoc10/b/10b.ml
+++ src/aoc10/b/10b.ml
@@ -3,22 +3,21 @@ open Stdio
 
 let adapter_array input =
   let input = List.sort input ~compare:Int.compare in
-  let test a b c = if a - b <=3 then c else 0 in
+  let sub_guard a b c = if a - b <= 3 then c else 0 in
 
   let rec loop x0 x1 x2 s0 s1 s2 rest =
     match rest with
     | [] -> s2
     | x3 :: xs ->
-      let scur = s2 + test x3 x1 (s1 + test x3 x0 s0) in
+      let scur = s2 + sub_guard x3 x1 (s1 + sub_guard x3 x0 s0) in
       loop x1 x2 x3 s1 s2 scur xs
   in
   match input with
-  | x1 :: x2 :: xs -> begin
+  | x1 :: x2 :: xs -> (
       let s0 = 1 in
       let s1 = 1 in
-      let s2 = s1 + test x1 0 1 in
-      loop 0 x1 x2 s0 s1 s2 xs
-    end
+      let s2 = s1 + sub_guard x1 0 1 in
+      loop 0 x1 x2 s0 s1 s2 xs)
   | _ -> assert false
 
 let () =
